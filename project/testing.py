74cd22c4185451d31c32ce1a2f6f503b81de0d72
Sachin Pawar <39683597+Sachin880699@users.noreply.github.com>
2021-06-29 10:07:45 +0530

    Delete testing.py

--- project/testing.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-sender_mail = 'sachin.pawar7802@gmail.com'
-receivers_mail = ["sachinnihcas373@gmail.com"]
-message = "Too many failed login attempts"
-password = "mymarathiamuchi"
-smtpObj = smtplib.SMTP('gmail.com', 587)
-smtpobj.login(sender_mail, password)
-smtpObj.sendmail(sender_mail, receivers_mail, message)
